d54194a65867e944d73e146f1478d54b5afcec3c
patelrashiii <raship7781@gmail.com>
2026-09-21 10:28:32 +0530

    Fix formatting of console log statement

--- HTTPMODULE/Day-08.js
+++ HTTPMODULE/Day-08.js
@@ -9,5 +9,5 @@ const server=http.createServer((req,res)=>{
     res.end();
 })
 server.listen(3001,()=>{
- console.log("Server is running on port 3001");    
+ console.log("Server is running on port 3001");   
 })
