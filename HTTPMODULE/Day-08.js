b9a73c344a98b7560b133d0fde5480e0c6c939b2
patelrashiii <raship7781@gmail.com>
2026-09-21 10:29:02 +0530

    Change name in response from Ritu Rai to Rashi Patel

--- HTTPMODULE/Day-08.js
+++ HTTPMODULE/Day-08.js
@@ -3,7 +3,7 @@
 import http from "http";
 const server=http.createServer((req,res)=>{
     res.writeHead(200,{"Content-Type":"text/html"});
-    res.write("<h1>Name: Ritu Rai</h1>")
+    res.write("<h1>Name: Rashi Patel</h1>")
     res.write("<h1>Welcome to my server</h1>"); 
     // res.write("HEllo World");
     res.end();
